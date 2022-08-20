38feb75a80c7b60d92952c9eb66e3579c8e86adc
gilberto <gsqwow@gmail.com>
2022-08-20 00:24:50 -0600

    Add content div and call start from home.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import {start} from './Home';
+import './style.css';
+
+const body = document.body;
+export const content = document.createElement('div');
+content.className = 'content';
+body.appendChild(content);
+
+start();
