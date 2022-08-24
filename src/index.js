c07f018377b0e26067369a49005d010899e733a2
gilberto <gsqwow@gmail.com>
2022-08-24 08:21:12 -0600

    Add eventlisteners to nav links

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {start} from './Home';
+import {start, removeHome, halfStart} from './Home';
+import {startContact, removeContact} from './contact';
 import './style.css';
 
 const body = document.body;
@@ -6,4 +7,37 @@ export const content = document.createElement('div');
 content.className = 'content';
 body.appendChild(content);
 
+
+
 start();
+
+const contact = document.querySelector('#contact');
+contact.addEventListener('click', function() {
+    const liContact = document.querySelector('#contact');
+    const liHome = document.querySelector('#home');
+    if (liHome.className == 'active'){
+        removeHome();
+    }
+
+    liContact.className = 'active';
+
+    startContact();
+
+});
+
+const home = document.querySelector('#home');
+home.addEventListener('click', function() {
+    const liHome = document.querySelector('#home');
+    const liContact = document.querySelector('#contact');
+    if (liContact.className == 'active') {
+        removeContact();
+        liContact.classList.toggle('active');
+    }
+
+    if (!(liHome.className == 'active')){
+        halfStart();
+    }
+
+
+});
+
