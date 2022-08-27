2a364cf0075cbfefaeeeb21171ee1327a3a31988
gilberto <gsqwow@gmail.com>
2022-08-26 21:38:02 -0600

    Add dish divs with img

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,6 @@
 import {content} from './index';
+import firstSalad from './img/firstSalad.png';
+import pastaClam from './img/pastaClam.png';
 
 const main = document.createElement('div');
 
@@ -6,6 +8,32 @@ const mainMenu = () => {
     main.className = 'main-menu';
     content.appendChild(main);
 }
+
+const makeSalad = () => {
+    const saladDiv = document.createElement('div');
+    saladDiv.className = 'salad-div';
+
+    const saladImg = new Image();
+    saladImg.src = firstSalad;
+    saladImg.className = 'first-salad';
+
+    main.appendChild(saladDiv);
+    saladDiv.appendChild(saladImg);
+}
+
+const makePasta = () => {
+    const pastaDiv = document.createElement('div');
+    pastaDiv.className = 'pasta-div';
+
+    const pastaImg = new Image();
+    pastaImg.src = pastaClam;
+    pastaImg.className = 'pasta-clam';
+
+    main.appendChild(pastaDiv);
+    pastaDiv.appendChild(pastaImg);
+}
+
+
 
 const halfMenu = () => {
     const liMenu = document.querySelector('#menu');
@@ -15,6 +43,8 @@ const halfMenu = () => {
 
 const startMenu = () => {
     mainMenu();
+    makeSalad();
+    makePasta();
 }
 
 const removeMenu = () => {
