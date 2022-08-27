363be0021c6006c598ac4b5f53dcac42b93c9790
gilberto <gsqwow@gmail.com>
2022-08-26 20:22:16 -0600

    Add basic start, halfstart, remove functions

--- src/Menu.js
+++ src/Menu.js
@@ -1,2 +1,30 @@
 import {content} from './index';
 
+const main = document.createElement('div');
+
+const mainMenu = () => {
+    main.className = 'main-menu';
+    content.appendChild(main);
+}
+
+const halfMenu = () => {
+    const liMenu = document.querySelector('#menu');
+    liMenu.className = 'active';
+    content.appendChild(main);
+}
+
+const startMenu = () => {
+    mainMenu();
+}
+
+const removeMenu = () => {
+    const liMenu = document.querySelector('#menu');
+    liMenu.className = '';
+    content.removeChild(main);
+}
+
+export {
+    startMenu,
+    halfMenu,
+    removeMenu
+}
